31f292b6abc63f73c2905b9488246175369acd2d
ricardofares <ricardoxdd28@outlook.com>
2023-06-11 15:00:39 -0300

    Liking a post is working correctly.

---
+++ backend/config/messages.js
@@ -0,0 +1,34 @@
+module.exports = {
+  "EMAIL_NOT_SPECIFIED": {
+    code: "E001",
+    message: "O e-mail não foi especificado.",
+  },
+  "PASSWORD_NOT_SPECIFIED": {
+    code: "E002",
+    message: "A senha não foi especificada.",
+  },
+  "EMAIL_NOT_REGISTERED": {
+    code: "E003",
+    message: "O usuário com o e-mail especificado não cadastrado",
+  },
+  "PASSWORD_INCORRECT": {
+    code: "E004",
+    message: "A senha informada está incorreta.",
+  },
+  "USER_NOT_SPECIFIED": {
+    code: "E005",
+    message: "O usuário não foi especificado.",
+  },
+  "CONTENT_NOT_SPECIFIED": {
+    code: "E006",
+    message: "O conteúdo não foi especificado.",
+  },
+  "CONTENT_EMPTY": {
+    code: "E007",
+    message: "O conteúdo não pode ser vazio.",
+  },
+  "POST_IDENTIFIER_NOT_SPECIFIED": {
+    code: "E008",
+    message: "O identificador da postagem não foi especificado.",
+  },
+};
