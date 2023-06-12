624646d6ab3bfd99926d279aad0301f446c7f4c2
ricardofares <ricardoxdd28@outlook.com>
2023-06-12 15:45:53 -0300

    Added the migration that alter the table 'posts' adding the 'reply_to' column.

---
+++ backend/migrations/20230611193905_alter_table_posts.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.table('posts', table => {
+    table.integer('reply_to').references('id').inTable('posts');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.table('posts', table => {
+    table.dropColumn('reply_to');
+  });
+};
